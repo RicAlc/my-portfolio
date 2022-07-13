4d1b0a6bc04e26bd096cd7ed90fe1f9899e4ab7b
RicAlc <alcala.ricardoc@gmail.com>
2022-07-13 18:55:45 -0500

    24. Agregando componente MailMeForm

---
+++ src/components/MailMeForm.jsx
@@ -0,0 +1,45 @@
+function MailMeForm() {
+  return (
+    <form
+      onSubmit='https://formsubmit.co/ajax/richialexis@live.com.mx'
+      method='POST'
+      className='mail-me'
+    >
+      <fieldset>
+        <legend className='mail-me__title'>Enviar un correo</legend>
+        <p>
+          <label htmlFor='email'>Correo</label>
+          <input
+            required
+            type='email'
+            name='correo'
+            id='email'
+            placeholder='correo@ejemplo.com'
+          />
+        </p>
+        <p>
+          <label htmlFor='subject-choose'>Asunto</label>
+          <input list='subjects' id='subject-choose' name='subjects' required />
+          <datalist id='subjects'>
+            <option value='Diseño web' />
+            <option value='Contaco' />
+            <option value='Mensaje personal' />
+          </datalist>
+        </p>
+        <p>
+          <label htmlFor='message'>Mensaje</label>
+          <textarea
+            required
+            placeholder='Tu mensaje aquí'
+            name='message'
+            id='message'
+            cols='45'
+            rows='6'
+          ></textarea>
+        </p>
+      </fieldset>
+      <button className='btn mail-me__submmit'>Enviar</button>
+    </form>
+  );
+}
+export default MailMeForm;
