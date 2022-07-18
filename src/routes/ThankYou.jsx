61a5cc97e89a41f42e36f394fe461c9b2c2369d6
RicAlc <alcala.ricardoc@gmail.com>
2022-07-18 00:16:42 -0500

    34. Agregando pagina de agradecimiento por contacto

---
+++ src/routes/ThankYou.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function ThankYou() {
+  return (
+    <>
+      <h1 className='thank-you'>
+        Gracias por contactarte conmigo, te responderé tan pronto pueda.
+      </h1>
+    </>
+  );
+}
