ddfa2d674979ab9dd3adc21905306c674a803cc8
RicAlc <alcala.ricardoc@gmail.com>
2022-07-26 17:56:13 -0500

    43. Reparando error visual al hacer zoom menos

--- src/routes/PageNotFound.jsx
+++ src/routes/PageNotFound.jsx
@@ -3,7 +3,7 @@ import error404 from '../assets/notfound.png';
 
 export function PageNotFound() {
   return (
-    <>
+    <div className='notfound section'>
       <h1 className='notfound__title'>
         No econtramos esta la página que estás buscando
       </h1>
@@ -12,6 +12,6 @@ export function PageNotFound() {
         className='notfound__img'
         alt='Ilustración página no encontrada'
       />
-    </>
+    </div>
   );
 }
